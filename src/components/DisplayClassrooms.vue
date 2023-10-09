f23362cccc7fe3d47fc5ce8c9a0a525f47deff2a
ElF4TboY <thomasmarchand63@gmail.com>
2023-10-09 10:13:58 +0200

    DisplayStudents works good

--- src/components/DisplayClassrooms.vue
+++ src/components/DisplayClassrooms.vue
@@ -7,23 +7,39 @@
         :key="classroom.id"
       >
         {{ classroom.name }}
+        <button :id="classroom.id" @click="toggle">open</button>
+        <div :class="classroom.classId"></div>
+        <DisplayStudents
+          v-if="isShow && target === classroom.id"
+          :className="classroom.classId"
+        />
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { defineAsyncComponent, onMounted, ref } from "vue";
 
 const classrooms = ref([]);
+const isShow = ref(false);
+const target = ref("");
+
+const toggle = (e) => {
+  target.value = e.target.id;
+  isShow.value = !isShow.value;
+};
 
 onMounted(async () => {
   try {
     const response = await fetch("http://localhost:3004/classrooms");
     classrooms.value = await response.json();
-    console.log(classrooms.value);
   } catch (err) {
     console.log(err);
   }
 });
+
+const DisplayStudents = defineAsyncComponent(() =>
+  import("./DisplayStudents.vue")
+);
 </script>
